49398ba8f19772b503a099aeb3d0147d0123d143
Zhen Li <zhenl@netflix.com>
2017-06-16 09:15:57 -0700

    Adding K nearest neighbor

--- knearest.py
+++ knearest.py
@@ -17,6 +17,6 @@ clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 print(accuracy)
 
-example_measures = np.array([4,2,1,1,1,2,3,2,1])
-prediction = clf.predict(example_measures)
+example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,1,2,3,4,1]])
+prediction = clf.predict(example_measures.reshape(len(example_measures),-1))
 print(prediction)

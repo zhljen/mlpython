c3990c7ced31a8e3ed1cd7e6c20d39604086aad5
Zhen Li <zhenl@netflix.com>
2017-06-16 09:12:26 -0700

    Adding K nearest neighbor

---
+++ knearest.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors
+import pandas as pd
+
+#https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/
+df = pd.read_csv('datasets/breast-cancer-wisconsin.data')
+df.replace('?',-99999, inplace=True)
+df.drop(['id'],1,inplace=True)
+#you can drop the nas as well
+
+X = np.array(df.drop(['class'],1))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+example_measures = np.array([4,2,1,1,1,2,3,2,1])
+prediction = clf.predict(example_measures)
+print(prediction)

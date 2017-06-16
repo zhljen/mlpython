61a5306cfc324b89e730d9589203a2a68f2266f6
Zhen Li <zhenl@netflix.com>
2017-06-16 09:16:40 -0700

    adding model serialization

--- regression.py
+++ regression.py
@@ -1,8 +1,13 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import  style
+style.use('ggplot')
+
+import pickle
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close', 'Adj. Volume']]
@@ -10,6 +15,9 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 #features
 df = df[['Adj. Close','HL_PCT','PCT_change', 'Adj. Volume']]
+# print("The current df 1 ")
+# with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
+#     print(df)
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
@@ -17,25 +25,52 @@ df.fillna(-99999, inplace=True)
 # 10% out of the data points
 forecast_out = int(math.ceil(0.01*len(df)))
 
+
 print(forecast_out)
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
-
+#
+# with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
+#     print(df)
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
-#df.dropna(inplace=True)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+df.dropna(inplace=True)
 y = np.array(df['label'])
 print(len(X), len(y))
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
 # linear is good
-clf = LinearRegression()
-#try to use svm, which is quite worse
-#clf = svm.SVR(kernel='poly')
-clf.fit(X_train, y_train)
+# clf = LinearRegression(n_jobs=-1)
+# #try to use svm, which is quite worse
+# #clf = svm.SVR(kernel='poly')
+# clf.fit(X_train, y_train)
+# with open('linearregression.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+#saving the classifier to avoid training time
 accuracy = clf.score(X_test, y_test)
 
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
 
-print(accuracy)
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.ylabel('Price')
+plt.show()
 # print (df.head())
